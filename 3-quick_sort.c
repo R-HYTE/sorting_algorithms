49cec668f06d91a1b1dda4f5d5e7472faae7fa9e
R_HYTE <polycarpwathuta39@gmail.com>
2023-11-22 16:25:42 +0300

    Changed the design

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+void swap(int *array, ssize_t index1, ssize_t index2);
+void quick_sort(int *array, size_t size);
+void quicksort_recursion(int *array, ssize_t first, ssize_t last, int size);
+int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size);
+
 /**
  * swap - Swaps the positions of two elements in an array.
  * @array: The array.
@@ -15,6 +20,38 @@ void swap(int *array, ssize_t index1, ssize_t index2)
 }
 
 /**
+ * quick_sort - Prepares the terrain for the QuickSort algorithm.
+ * @array: The array.
+ * @size: The size of the array.
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quicksort_recursion(array, 0, size - 1, size);
+}
+
+/**
+ * quicksort_recursion - QuickSort algorithm implementation.
+ * @array: The array.
+ * @first: Index of the first array element.
+ * @last: Index of the last array element.
+ * @size: The size of the array.
+ */
+void quicksort_recursion(int *array, ssize_t first, ssize_t last, int size)
+{
+	ssize_t pivot_index = 0;
+
+	if (first < last)
+	{
+		pivot_index = lomuto_partition(array, first, last, size);
+		quicksort_recursion(array, first, pivot_index - 1, size);
+		quicksort_recursion(array, pivot_index + 1, last, size);
+	}
+}
+
+/**
  * lomuto_partition - Lomuto partition sorting scheme implementation.
  * @array: The array.
  * @first: Index of the first array element.
@@ -24,12 +61,12 @@ void swap(int *array, ssize_t index1, ssize_t index2)
  */
 int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
 {
-	int pivot = array[last];
+	int pivot_value = array[last];
 	ssize_t current = first, finder;
 
 	for (finder = first; finder < last; finder++)
 	{
-		if (array[finder] < pivot)
+		if (array[finder] < pivot_value)
 		{
 			if (array[current] != array[finder])
 			{
@@ -47,36 +84,4 @@ int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
 	}
 
 	return (current);
-}
-
-/**
- * _quicksort - QuickSort algorithm implementation.
- * @array: The array.
- * @first: Index of the first array element.
- * @last: Index of the last array element.
- * @size: The size of the array.
- */
-void _quicksort(int *array, ssize_t first, ssize_t last, int size)
-{
-	ssize_t position = 0;
-
-	if (first < last)
-	{
-		position = lomuto_partition(array, first, last, size);
-		_quicksort(array, first, position - 1, size);
-		_quicksort(array, position + 1, last, size);
-	}
-}
-
-/**
- * quick_sort - Prepares the terrain for the QuickSort algorithm.
- * @array: The array.
- * @size: The size of the array.
- */
-void quick_sort(int *array, size_t size)
-{
-	if (!array || size < 2)
-		return;
-
-	_quicksort(array, 0, size - 1, size);
 }
